6f80078d2ea5e304345b1d991addd13c159fe134
gavin_m_douglas <gavinmdouglas@gmail.com>
2024-01-22 14:11:07 -0500

    flake8 fixes

--- predict_sav.py
+++ predict_sav.py
@@ -1,12 +1,12 @@
+import torch
 import numpy as np
 import os
 import matplotlib.pyplot as plt
-from progressBar import *
+from progressBar import printProgressBar
 from Bio import SeqIO
 from argparse import ArgumentParser
-from Ember3D import *
-from lddt import *
-
+from Ember3D import Ember3D
+from lddt import lddt
 
 parser = ArgumentParser()
 parser.add_argument('-i', '--input', dest="input", type=str, required=True)
@@ -21,10 +21,10 @@ parser.add_argument('--t5-dir', dest='t5_dir', default="./ProtT5-XL-U50/", type=
 
 parser.add_argument('--test-sites', dest='test_sites', default=None, type=str,
                     help='Optional input table specifying which amino acid positions '
-                          'to mutate, and which substitutions to make. '
-                          'Table should not have a header, and should be tab-delimited with two columns. '
-                          'The first column should be the AA position number (starting at 1, although not all positions need be present). '
-                          'The second column must be comma-delimited single-letter amino acid codes to test for substitutions at that position.')
+                    'to mutate, and which substitutions to make. '
+                    'Table should not have a header, and should be tab-delimited with two columns. '
+                    'The first column should be the AA position number (starting at 1, although not all positions need be present). '
+                    'The second column must be comma-delimited single-letter amino acid codes to test for substitutions at that position.')
 
 args = parser.parse_args()
 
@@ -34,7 +34,7 @@ if not os.path.isdir(args.output_dir):
 
 Ember3D = Ember3D(args.model_checkpoint, args.t5_dir, args.device)
 
-for i,record in enumerate(SeqIO.parse(args.input, "fasta")):
+for i, record in enumerate(SeqIO.parse(args.input, "fasta")):
     id = record.id
     seq = str(record.seq)
     seq_l = list(seq)
@@ -58,7 +58,6 @@ for i,record in enumerate(SeqIO.parse(args.input, "fasta")):
         os.makedirs(dist_dir)
 
     # Wild-type prediction
-
     print("{}\t{}".format(i, id))
     with torch.cuda.amp.autocast():
         result = Ember3D.predict(seq)
@@ -74,9 +73,7 @@ for i,record in enumerate(SeqIO.parse(args.input, "fasta")):
         if args.save_distance_array:
             np.save("{}/{}_distances.npy".format(dist_dir, id), wild_type_distance_map)
 
-
     # Mutation predictions
-
     if args.test_sites is None:
         # Run original approach (testing all possible substitutions if test_sites file not specified).
         aa_list = list("ACDEFGHIKLMNPQRSTVWY")
@@ -84,7 +81,7 @@ for i,record in enumerate(SeqIO.parse(args.input, "fasta")):
         mutation_log = os.path.join(sample_dir, id + "_mutation_log.txt")
         total = length * 19
         counter = 0
-        printProgressBar(0, total, prefix = 'Mutants:', suffix = 'Complete', length = 50)
+        printProgressBar(0, total, prefix='Mutants:', suffix='Complete', length=50)
         with open(mutation_log, "w") as f:
             for seq_idx in range(length):
                 for aa in aa_list:
@@ -115,7 +112,7 @@ for i,record in enumerate(SeqIO.parse(args.input, "fasta")):
                         f.write("{} {} {} {}\n".format(seq_l[seq_idx], seq_idx + 1, aa, struct_diff))
 
                         counter += 1
-                        printProgressBar(counter, total, prefix = 'Mutants:', suffix = 'Complete', length = 50)
+                        printProgressBar(counter, total, prefix='Mutants:', suffix='Complete', length=50)
 
         print("")
 
@@ -135,7 +132,7 @@ for i,record in enumerate(SeqIO.parse(args.input, "fasta")):
         mutation_matrix = np.ones((20, length))
         mutation_log = os.path.join(sample_dir, id + "_mutation_log.txt")
         counter = 0
-        printProgressBar(0, total, prefix = 'Mutants:', suffix = 'Complete', length = 50)
+        printProgressBar(0, total, prefix='Mutants:', suffix='Complete', length=50)
         with open(mutation_log, "w") as f:
             for seq_pos in sorted(list(subs_to_test.keys())):
                 seq_idx = seq_pos - 1
@@ -144,7 +141,7 @@ for i,record in enumerate(SeqIO.parse(args.input, "fasta")):
                     # Throw error if aa not found in possible set.
                     if aa not in possible_aa:
                         raise ValueError("Invalid amino acid code {} found in test_sites file.".format(aa))
-                    
+
                     # Throw error if one of the specified AAs is the actual AA at this position.
                     if aa == seq_l[seq_idx]:
                         raise ValueError("Specified substitution {} at position {} is the same as the wild-type residue.".format(aa, seq_pos))
@@ -174,7 +171,7 @@ for i,record in enumerate(SeqIO.parse(args.input, "fasta")):
                         f.write("{} {} {} {}\n".format(seq_l[seq_idx], seq_idx + 1, aa, struct_diff))
 
                         counter += 1
-                        printProgressBar(counter, total, prefix = 'Mutants:', suffix = 'Complete', length = 50)
+                        printProgressBar(counter, total, prefix='Mutants:', suffix='Complete', length=50)
 
         print("")
 
@@ -193,9 +190,9 @@ for i,record in enumerate(SeqIO.parse(args.input, "fasta")):
         cbar = plt.colorbar(shrink=0.5)
         cbar.set_label("similarity to wild-type")
         plt.title("Effect of point mutations")
-    
-        plt.subplots_adjust(top = 1, bottom = 0, right = 1, left = 0, hspace = 0, wspace = 0)
-        plt.margins(0,0)
 
-        plt.savefig("{}/{}_mutation_matrix.png".format(sample_dir, id), bbox_inches = 'tight', pad_inches = 0)
+        plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
+        plt.margins(0, 0)
+
+        plt.savefig("{}/{}_mutation_matrix.png".format(sample_dir, id), bbox_inches='tight', pad_inches=0)
         plt.clf()
